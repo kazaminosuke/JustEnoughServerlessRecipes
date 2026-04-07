90cd5737d1e31b30c5bb52f37e12c0798298dda8
Draconic Velum <87547775+DraconicVelum@users.noreply.github.com>
2026-04-07 21:32:33 +0100

    Add files via upload

---
+++ src/main/java/com/draconicvelum/justenoughserverlessrecipes/JustEnoughServerlessRecipesLog.java
@@ -0,0 +1,11 @@
+package com.draconicvelum.justenoughserverlessrecipes;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class JustEnoughServerlessRecipesLog {
+    public static final Logger LOGGER = LoggerFactory.getLogger("JESR");
+
+    private JustEnoughServerlessRecipesLog() {
+    }
+}
